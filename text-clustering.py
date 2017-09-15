534ba117ba272de562594509ef207dcd622e5c50
lannle <le_l1@denison.edu>
2017-09-14 23:22:38 -0400

    Vectorization

--- text-clustering.py
+++ text-clustering.py
@@ -1,6 +1,8 @@
 def readData(textFile):
-    file = open(textFile,"r")
-    data = file.read().splitlines() 
+    file = open(textFile)
+    data = []
+    for line in file:
+        data.append(line)
     return data
 
 
@@ -16,24 +18,47 @@ def getDistinctWords(textMsgDictionary):
     distinctWordList = []
     for user in  textMsgDictionary:
         for msg in textMsgDictionary[user]:
-
-            #remove unwanted characters
-
-            for char in msg:
-                if char in ".?!":
-                    msg = msg.replace(char, " ")
-
-            # stripped msg 
-            wordList = msg.split(" ")
+            wordList = textMsgConversion(msg)
             for word in wordList:
                 if word not in distinctWordList:
                     distinctWordList.append(word)
     return distinctWordList
+
+def textMsgConversion(msg):
+    #remove unwanted characters
+    for char in msg:
+        if char in ".?!,":
+            msg = msg.replace(char, " ")
+
+    # stripped msg
+    msg = msg.lower()
+    wordList = msg.split(" ")
+    return wordList
+            
+def textVectorization (distinctWordList,msg):
+    vector = []
+    wordList = textMsgConversion(msg)
+    for i in range(len(distinctWordList)):
+        if distinctWordList[i] in wordList:
+            vector.append(1)
+        else:
+            vector.append(0)
+    return vector
+
+def vectorizedMatrix (distinctWordList,textMsgDictionary):
+    vectorizedMatrix = []
+    for user in  textMsgDictionary:
+        for msg in textMsgDictionary[user]:
+            vector = textVectorization(distinctWordList,msg)
+            vectorizedMatrix.append(vector)
+    return vectorizedMatrix
+                     
             
         
+#data= readData("test.txt")
+data= readData("test1.txt")
 
-#def textParsing(textfile):
-#print(readData("parsedxmlnew.txt"))
-data= readData("test.txt")
 textMsgDictionary = textParsing(data)
-print(getDistinctWords(textMsgDictionary))
+distinctWordList = getDistinctWords(textMsgDictionary)
+print(vectorizedMatrix (distinctWordList,textMsgDictionary))
+
